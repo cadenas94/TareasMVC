690127e0d491399e5f363314f6bf30f99a3e7666
DanielCadenas <danicadenascab@gmail.com>
2023-07-17 16:48:13 +0200

    Add project files.

---
+++ TareasMVC/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using TareasMVC.Entidades;
+
+namespace TareasMVC
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Usar API Fluente
+            //modelBuilder.Entity<Tarea>().Property(testc => testc.Titulo).HasMaxLength(250).IsRequired();
+        }
+        public DbSet<Tarea> Tareas { get; set; }
+        public DbSet<Paso> Pasos { get; set; }
+        public DbSet<ArchivoAdjunto> ArchivosAdjuntos { get; set; }
+    }
+}
---
+++ TareasMVC/Entidades/Paso.cs
@@ -0,0 +1,13 @@
+﻿namespace TareasMVC.Entidades
+{
+    public class Paso
+    {
+        public Guid Id { get; set; }
+        public int TareaId { get; set; } //Al usar la combinacion de la clase Tarea + Id, usa una convencion de EF para definir la foreign key
+        public Tarea Tarea { get; set; } //Permite cargar la data relacionada del paso 
+        public string Descripcion { get; set; }
+        public bool Realizado { get; set; }
+        public int Orden { get; set; }
+        
+    }
+}
